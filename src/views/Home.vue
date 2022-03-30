0f2c0c2156a6175412b550267dc79be31eecfe48
thanatadcs <thanatad.cs@gmail.com>
2022-03-31 01:25:39 +0700

    Change route params to be based on video id and make resume functionality work with the change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,7 +22,7 @@
               @mouseleave="setMouseHoverLeave(index)"
             >
               <!-- Video Link -->
-              <router-link class="routerLink" :to="'/video/' + video.filename">
+              <router-link class="routerLink" :to="'/video/' + video.id">
                 <div @click="spinner = true">
                   <v-img
                     lazy-src="https://i.imgur.com/XJRowdx.png"
--- src/views/Video.vue
+++ src/views/Video.vue
@@ -27,11 +27,13 @@ export default {
   computed: {
     getVideo() {
       return store.state.videos.find(
-        (video) => video.filename.localeCompare(this.$route.params.id) === 0
+        (video) => video.id == this.$route.params.id
       );
     },
   },
   created() {
+    console.log(store.state.videos);
+    console.log(this.getVideo.title);
     this.$set(this, "title", this.getVideo.title);
     this.videoOptions.sources[0].src = this.getVideo.link;
   },

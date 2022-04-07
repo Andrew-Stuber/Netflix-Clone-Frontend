562c0456be65feb1d17501eb3150646cd55bc3ff
Andy-stuber <75411977+Andy-stuber@users.noreply.github.com>
2022-04-08 00:43:32 +0700

    redirect to about page with names

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,12 @@
 <template>
   <v-container>
     <div class="about">
-      <h1>This is an about page</h1>
+      <h1>The Sussy Bakas:</h1>
+      <h2>Thanatad Anukoolprasert 6280199</h2>
+      <h2>Yuttasart Viratpan 6281370</h2>
+      <h2>Thomas Timothy Saran Harris 6180244</h2>
+      <h2>Andrew Stuber 6281425</h2>
+      <h2>Thanaphat Chanparn 6281462</h2>
     </div>
   </v-container>
 </template>
